57d474c02c1da68499a00c83a656c1b926e3f656
alexanderfield <alexander.field@yahoo.com>
2015-09-12 06:34:47 -0400

    interesting stuff

--- index.js
+++ index.js
@@ -5,3 +5,47 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
     $mdSidenav(menuId).toggle();    
   };
 }]);
+
+(function () {
+  'use strict';
+  angular
+      .module('StarterApp')
+      .controller('DemoCtrl', DemoCtrl);
+  function DemoCtrl ($timeout, $q, $log) {
+    var self = this;
+    self.simulateQuery = false;
+    self.isDisabled    = false;
+    // list of `state` value/display objects
+    self.interests       = loadAll();
+    self.querySearch   = querySearch;
+
+
+    function querySearch (query) {
+      var results = query ? self.interests.filter( createFilterFor(query) ) : self.interests,
+          deferred;
+     
+      return results;
+      
+    }
+
+
+    function loadAll() {
+      var allInterests = 'Code, Crypto, Design, Health, Wealth';
+      return allInterests.split(/, +/g).map( function (interest) {
+        return {
+          value: interest.toLowerCase(),
+          display: interest
+        };
+      });
+    }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(state) {
+        return (interest.value.indexOf(lowercaseQuery) === 0);
+      };
+    }
+  }
+})();
